5b5d06b77e735fa65b25454b46a6ca223fee3ed9
machado0 <medmachado27@gmail.com>
2025-02-21 20:08:10 -0300

    fix: tornando titulo da tabela pauta unique

--- src/main/resources/db/migration/V1__create_pauta.sql
+++ src/main/resources/db/migration/V1__create_pauta.sql
@@ -1,6 +1,6 @@
 CREATE TABLE pauta (
     id BIGSERIAL PRIMARY KEY,
-    titulo VARCHAR(255) NOT NULL,
+    titulo VARCHAR(255) UNIQUE NOT NULL,
     descricao VARCHAR(255) NOT NULL,
     tempo_encerramento TIMESTAMP NOT NULL
 );
