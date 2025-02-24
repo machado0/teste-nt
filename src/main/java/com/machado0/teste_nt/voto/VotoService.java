c1da76be14cf004d5d3f84515ecdc4136df64560
machado0 <medmachado27@gmail.com>
2025-02-24 09:27:57 -0300

    refactor: ajuste no listarResultados para adicionar paginação

--- src/main/java/com/machado0/teste_nt/voto/VotoService.java
+++ src/main/java/com/machado0/teste_nt/voto/VotoService.java
@@ -8,7 +8,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.OffsetDateTime;
-import java.util.List;
 
 import static java.util.Objects.nonNull;
 
